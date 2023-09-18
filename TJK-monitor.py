7549a5f935224269a98871064f3ac3d29da1cc13
Alf Köhn-Seemann <alf.koehn@posteo.net>
2023-09-18 18:55:01 +0200

    feat: convert shot in get_shot_path to int if string

--- TJK-monitor.py
+++ TJK-monitor.py
@@ -37,6 +37,7 @@ def get_shot_path( shot ):
         print('get_shot_path: warning, argument <shot> should be int, is string')
         print('               will be converted to int, shot = ', shot)
         print('               might trigger some side effects')
+        shot = int(shot)
 
     possible_folders    = [ 
                             '/data6/', 
