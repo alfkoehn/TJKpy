1ca31683c722df3b148b3b6fb00d5d9ecc5fd7ea
Alf Köhn <alf.koehn@posteo.net>
2023-09-26 18:12:55 +0200

    fix: bug in get_tjkmonitor_path

--- tjk_shotview.py
+++ tjk_shotview.py
@@ -80,12 +80,11 @@ def get_tjkmonitor_datapath(shot):
     else:
         shot_path   = tjk.get_shot_path(shot)
         if isinstance(shot_path, str):
-            shot_path = Path(shot_path + "interferometer/")
-            #shot_path += 'interferometer/'
+            shot_path   = Path(shot_path + "interferometer/")
         elif Path.exists(shot_path):
-            shot_path = shot_path / "interferometer/"
+            shot_path   = shot_path / "interferometer/"
         else:
-            return shot_path    = errValue
+            shot_path   = errValue
 
     return shot_path
     #}}}
